ad2101daffa2b80c851fc7348e5d6d83de998b6b
Jackon Yang <i@jackon.me>
2022-07-06 23:31:35 +0800

    init pipeline manage.py

---
+++ src/manage.py
@@ -0,0 +1,74 @@
+import sys
+import argparse
+import os
+import json
+
+pipeline_choices = [
+    'gen-notes-md',
+]
+
+
+def default_pipe_runner_func(**kwargs):
+    print('pipe runner is not defined. user args: %s' % json.dumps(kwargs, indent=4))
+    return 1
+
+
+def load_pipeline_runner(pipe_name):
+    return default_pipe_runner_func
+
+
+def load_ini():
+    raise NotImplementedError
+
+
+def init_argparser():
+    parser = argparse.ArgumentParser(description='Commond Line Interface for PaperPipe.')
+
+    parser.add_argument(
+        'pipeline', metavar='pipeline', type=str,
+        help=', '.join(pipeline_choices))
+
+    parser.add_argument(
+        '--ini', metavar='job_params', type=str,
+        default=None,
+        help='path to your job_config.ini file')
+
+    return parser
+
+
+def run_job(args):
+    err_no = 0
+
+    # init pipeline runner
+    pipeline = args.pipeline
+    pipe_runner_func = load_pipeline_runner(pipeline)
+
+    # init kwargs dict
+    if args.ini and os.path.exists(args.ini):
+        kwargs = load_ini(args.ini)
+    else:
+        kwargs = {}
+
+    # update kwargs if specified in command line args
+
+    # run the pipeline
+    err_no = pipe_runner_func(**kwargs)
+
+    if err_no is None:
+        raise ValueError('missing return code. pipeline: %s, runner function: %s, file: %s' %
+                         (pipeline, pipe_runner_func.__name__, pipe_runner_func.__code__.co_filename))
+
+    return err_no
+
+
+def main():
+    parser = init_argparser()
+    args = parser.parse_args()
+
+    err_no = run_job(args)
+    return err_no
+
+
+if __name__ == '__main__':
+    err_no = main()
+    sys.exit(err_no)
