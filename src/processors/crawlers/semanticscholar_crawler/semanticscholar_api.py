357d42463b5d6beafa412ed83a596b513c194373
Jackon Yang <i@jackon.me>
2023-05-05 22:36:11 +0800

    update fields

--- src/processors/crawlers/semanticscholar_crawler/semanticscholar_api.py
+++ src/processors/crawlers/semanticscholar_crawler/semanticscholar_api.py
@@ -10,12 +10,18 @@ logger = logging.getLogger(__name__)
 
 url_tmpl = 'https://api.semanticscholar.org/graph/v1/paper/%(pid)s/%(key)s?fields=%(fields)s&offset=%(offset)s&limit=%(limit)s'
 
-paper_fileds = ','.join([
+default_fields = [
+    'paperId',
     'title',
-    'authors',
+
+    'fieldsOfStudy',
+    's2FieldsOfStudy',
     'referenceCount',
     'citationCount',
-])
+    'year',
+
+    'authors',
+]
 
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -24,26 +30,28 @@ example_dir = os.path.join(CURRENT_DIR, 'example')
 
 def fetch_paper_references(pid, offset, limit, **kwargs):
     key = 'references'
-    return do_request(key, pid, offset, limit)
+    paper_fields = ','.join(default_fields)
+    return do_request(key, paper_fields, pid, offset, limit)
 
 
 def fetch_paper_citations(pid, offset, limit, **kwargs):
     key = 'citations'
-    return do_request(key, pid, offset, limit)
+    paper_fields = ','.join(default_fields)
+    return do_request(key, paper_fields, pid, offset, limit)
 
 
 @jcache
-def do_request(key, pid, offset, limit):
+def do_request(key, paper_fields,  pid, offset, limit):
     params = {
         'key': key,
         'pid': pid,
-        'fields': paper_fileds,
+        'fields': paper_fields,
         'offset': offset,
         'limit': limit,
     }
 
     url = url_tmpl % params
-    logger.info('requesting... key: %s, url: %s' % (key, url))
+    logger.debug('requesting... key: %s, url: %s' % (key, url))
     resp = requests.get(url)
     if resp.status_code != 200:
         return None
